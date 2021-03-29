dab150cc40420a3b5714f8516fb28c1ff96b6807
prem <prem.sherpa@codeimmersives.com>
2021-03-29 16:21:46 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+require('dotenv').config();
+var cors = require('cors');
+var mongoose = require('mongoose');
+// mongoose connector
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+const mongooseOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}
+mongoose.connect(process.env.MONGODB_URI, mongooseOptions)
+        .then(() => console.log('MONGODB CONNECTED'))
+        .catch(err => console.log(err));
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users/users');
+
+var app = express();
+
+// view engine setup
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/api', indexRouter);
+app.use('/api/users', usersRouter); 
+// app.listen(4000, () => console.log('Backend on port 4000'))
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
